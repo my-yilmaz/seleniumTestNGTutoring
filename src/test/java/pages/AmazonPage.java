db170bb75c9410c87b2f811e398ab4f3501ecd98
my-yilmaz <myyilmaz046@gmail.com>
2023-10-07 20:19:18 +0300

    Initial commit

---
+++ src/test/java/pages/AmazonPage.java
@@ -0,0 +1,22 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+
+public class AmazonPage {
+    public AmazonPage() {
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    @FindBy(xpath = "//*[@id='twotabsearchtextbox']")
+    public WebElement amazonSearch;
+
+    @FindBy(xpath = "//span[@class='a-color-state a-text-bold']")
+    public WebElement result;
+
+    @FindBy(xpath = "(//span[@class='a-size-medium a-color-base a-text-normal'])[1]")
+    public WebElement ilkUrun;
+}
---
+++ src/test/java/tests/TC_01.java
@@ -0,0 +1,24 @@
+package tests;
+
+import org.openqa.selenium.Keys;
+import org.testng.annotations.Test;
+import pages.AmazonPage;
+import utilities.ConfigReader;
+import utilities.Driver;
+
+public class TC_01 {
+    @Test
+    public void test01() {
+        //1- https://www.amazon.com/ sayfasina gidelim
+        Driver.getDriver().get(ConfigReader.getProperty("AmazonURL"));
+        //2- arama kutusunu locate edelim
+        //3- “Samsung headphones” ile arama yapalim
+        AmazonPage amazonPage = new AmazonPage();
+        amazonPage.amazonSearch.sendKeys("Samsung headphones", Keys.ENTER);
+        //4- Bulunan sonuc sayisini yazdiralim
+        System.out.println(amazonPage.result.getText());
+        //5- Ilk urunu tiklayalim
+        amazonPage.ilkUrun.click();
+        Driver.closeDriver();
+    }
+}
