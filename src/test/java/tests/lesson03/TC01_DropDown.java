7433b9b0ff3adeed5915c7256750261c306f7fd3
my-yilmaz <myyilmaz046@gmail.com>
2023-10-10 17:44:51 +0300

    muhammed v7

--- src/test/java/tests/lesson03/TC01_DropDown.java
+++ src/test/java/tests/lesson03/TC01_DropDown.java
@@ -1,5 +1,13 @@
 package tests.lesson03;
 
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+import pages.AmazonPage;
+import utilities.ConfigReader;
+import utilities.Driver;
+
 public class TC01_DropDown {
     /**
      * amazon'a gidip
@@ -7,4 +15,18 @@ public class TC01_DropDown {
      * Java aratalim
      * ve arama sonuclarinin Java icerdigini test edelim
      */
+
+    @Test
+    public void test01() {
+        Driver.getDriver().get(ConfigReader.getProperty("amazonURL"));
+        AmazonPage amazonPage = new AmazonPage();
+        Select select = new Select(amazonPage.allDropDownBox);
+        select.selectByVisibleText("Books");
+        //select.selectByIndex(5); //index ile
+        amazonPage.amazonSearch.sendKeys("java", Keys.ENTER);
+        SoftAssert softAssert = new SoftAssert();
+        softAssert.assertTrue(amazonPage.result.getText().contains("java"));
+        softAssert.assertAll();
+        Driver.closeDriver();
+    }
 }
