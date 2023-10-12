8a075e4bf5a9d467156cd2f2604f4e5b7244f0a9
my-yilmaz <myyilmaz046@gmail.com>
2023-10-12 10:10:15 +0300

    muhammed v10

---
+++ src/test/java/tests/lesson05/TC02_Actions.java
@@ -0,0 +1,15 @@
+package tests.lesson05;
+
+import org.testng.annotations.Test;
+
+public class TC02_Actions {
+    @Test
+    public void test01() {
+        //1- https://the-internet.herokuapp.com/context_menu sitesine gidelim
+        //2- Cizili alan uzerinde sag click yapalim
+        //3- Alert'te cikan yazinin "You selected a context menu" oldugunu test edelim.
+        //4- Tamam diyerek alert'i kapatalim
+        //5- Elemental Selenium linkine tiklayalim
+        //6- Acilan sayfada h1 taginda "Elemental Selenium" yazdigini test edelim
+    }
+}
